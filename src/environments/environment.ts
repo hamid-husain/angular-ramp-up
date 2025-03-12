311f0888197dec6edd584a69370c23c87fd9bfda
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-03-12 18:59:28 +0530

    setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyCEh2ljNim0nMYT1oLVWyzEiY3mNAICF7Q",
+        authDomain: "mini-social-network-a9d09.firebaseapp.com",
+        projectId: "mini-social-network-a9d09",
+        storageBucket: "mini-social-network-a9d09.firebasestorage.app",
+        messagingSenderId: "292788951541",
+        appId: "1:292788951541:web:9735a742ab95da571128bc"
+      }
+};
