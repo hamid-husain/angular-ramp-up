baf0a3bd7c1b4f9b3f6e9ee099db8b287850ae54
hamid-JTG <hamid.husain@joshtechnologygroup.com>
2025-03-18 17:53:23 +0530

    password-validator

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/auth/login/login.component';
-import { SignUpComponent } from './modules/auth/sign-up/sign-up.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
+import { SignUpComponent } from './modules/auth/components/sign-up/sign-up.component';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guard';
